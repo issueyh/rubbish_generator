3f59510a8a9526783b79f6763e98a3281da56ccc
issueyh <issueyh12921@gmail.com>
2021-06-06 22:32:29 +0800

    feat: add generator_rubbish.js

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const hbshelpers = require('handlebars-helpers')()
+const generateRubbish = require('./generator_rubbish')
 const app = express()
 const port = 3000
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
+app.use(express.urlencoded({ extended: true }))
+
 app.get('/', (req, res) => {
     res.render('index')
+})
+
+app.post('/', (req, res) => {
+    const character = req.body.character
+    const rubbish = generateRubbish(character)
+    res.render('index', { character, rubbish })
 })
 
 app.listen(port, () => {
